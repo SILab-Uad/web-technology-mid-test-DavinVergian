71298ddaaf9f6ef635c11e8a3e8dbb13cc0335a7
Dapinee <166325014+DavinVergian@users.noreply.github.com>
2024-10-22 15:22:51 +0700

    Attempt 1 - 08:45

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Panjanga dan pilihan tipe password yang dipilih
-const generatePassword = (length, options) => {
+export const generatePassword = (length, options) => {
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
@@ -13,9 +13,8 @@ const generatePassword = (length, options) => {
 
   // Kalo ga ada input
   if (charset === "") {
-    alert("Masukin Minimal Satu Pilihan Maas");
-    return "";
-  }
+    throw new Error('At least one character type must be selected.');
+    }
 
   // Membuat password
   let password = "";
@@ -26,26 +25,3 @@ const generatePassword = (length, options) => {
 
   return password;
 };
-
-// Mengyusun output user sesuai yang mereka pilih
-document.getElementById("generateBtn").addEventListener("click", () => {
-  const length = parseInt(document.getElementById("length").value, 10);
-  const options = {
-    includeUppercase: document.getElementById("includeUppercase").checked,
-    includeLowercase: document.getElementById("includeLowercase").checked,
-    includeNumbers: document.getElementById("includeNumbers").checked,
-    includeSpecialChars: document.getElementById("includeSpecialChars").checked,
-  };
-
-  // Menampilkan outputpassword
-  const password = generatePassword(length, options);
-  document.getElementById("passwordOutput").value = password; // Menampilkan password di input field
-});
-
-// Copypaste output
-document.getElementById("copyBtn").addEventListener("click", () => {
-  const passwordField = document.getElementById("passwordOutput");
-  passwordField.select();
-  document.execCommand("copy");
-  alert("Password copied to clipboard!");
-});
